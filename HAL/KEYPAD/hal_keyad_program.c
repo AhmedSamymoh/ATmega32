402eb4a3140feddb93b05acde0229f5d34d6e7c6
Ahmed Elnozahy <108683164+AhmedSamymoh@users.noreply.github.com>
2024-02-02 20:07:01 +0200

    Updating Keypad Driver

--- HAL/KEYPAD/hal_keyad_program.c
+++ HAL/KEYPAD/hal_keyad_program.c
@@ -16,7 +16,7 @@ uint8 btn_values[4][4] = {
 									{'1', '2', '3', '-'},
 									{'#', '0', '=', '+'}
                                  };
-void KEYPAD_init(){
+void KEYPAD_init(void){
 
 	uint8 i;
 	for(i = OUTPUT_START; i <= OUTPUT_END ; i++)
@@ -31,119 +31,49 @@ void KEYPAD_init(){
 		DIO_SetPinDirection(KEYPAD_PORT , i , PIN_INPUT);
 		DIO_SetPinValue(KEYPAD_PORT , i , PIN_HIGH);
 	}
-//		DDRA = 0b11110000;
-//		PORTA = 0b11111111;
-//		PINA = 0b11111111;
 }
 
-//uint8 KEYPAD_GetPressedKey(void)
-//{
-//	uint8  L_PressedKey = KEY_NOT_PRESSED , L_State;
-//	uint8 L_ColumnIndex = 0 , L_RowIndex = 0;
-//	static uint8 L_ColumnArr[4] = {COLUMN0_PIN ,COLUMN1_PIN ,COLUMN2_PIN ,COLUMN3_PIN};
-//	static uint8 L_RowArr[4] = {ROW0_PIN ,ROW1_PIN ,ROW2_PIN ,ROW3_PIN};
-//
-//	for(L_ColumnIndex = 0 ; L_ColumnIndex < 4 ; L_ColumnIndex++)
-//	{
-//		/*Activate the Current Column*/
-//		DIO_SetPinDirection(KEYPAD_PORT, L_ColumnArr[L_ColumnIndex] ,PIN_LOW);
-//		_delay_ms(10);
-//		for(L_RowIndex = 0 ; L_RowIndex < 4 ; L_RowIndex++)
-//		{
-//			/*Read the Current Row*/
-//			L_State = DIO_GetPinValue(KEYPAD_PORT , L_RowArr[L_RowIndex]);
-//
-//			/*Check if switch is pressed*/
-//			if(PIN_LOW == L_State)
-//			{
-//				L_PressedKey = btn_values[L_RowIndex][L_ColumnIndex];
-//				 /* Polling Wait until the switch is released*/
-//				while (PIN_LOW == L_State){
-//					L_State = DIO_GetPinValue(KEYPAD_PORT , L_RowArr[L_RowIndex]);
-//				}
-//				/*avoid bouncing*/
-//				_delay_ms(10);
-//
-//				return L_PressedKey;
-//
-//			}
-//		}
-//	}
-//
-//	return L_PressedKey;
-//}
+uint8 KEYPAD_GetPressedKey(void)
+{
+	uint8  L_PressedKey = KEY_NOT_PRESSED , L_State;
+	uint8 L_ColumnIndex = 0 , L_RowIndex = 0;
+	static uint8 L_ColumnArr[4] = {COLUMN0_PIN ,COLUMN1_PIN ,COLUMN2_PIN ,COLUMN3_PIN};
+	static uint8 L_RowArr[4] = {ROW0_PIN ,ROW1_PIN ,ROW2_PIN ,ROW3_PIN};
 
-//uint8 KPD_u8GetPressedKey(void)
-//{
-//
-//	uint8 LOC_u8Column;
-//
-//  uint8 LOC_u8Row;
-//
-//  /* Initialize the switch status to NOT Pressed                                                                     */
-//  uint8 LOC_u8Retrun = KEY_NOT_PRESSED;
-//
-//  /* Looping on columns of the Key Pad                                                                               */
-//  for (LOC_u8Column = 0 + OUTPUT_START; LOC_u8Column < OUTPUT_END; LOC_u8Column++)
-//  {
-//    /* Activate the Column                                                                                           */
-//	  DIO_SetPinValue(KEYPAD_PORT, LOC_u8Column, PIN_LOW);
-//
-//    /* Loop on the rows of the Key Pad                                                                               */
-//    for (LOC_u8Row = 0 + INPUT_START; LOC_u8Row < INPUT_END; LOC_u8Row++)
-//    {
-//      /* Check the status of the switch                                                                              */
-//      if (!DIO_GetPinValue(KEYPAD_PORT, LOC_u8Row))
-//      {
-//        /* Get the value of the currently pressed switch                                                             */
-//        LOC_u8Retrun = btn_values[LOC_u8Column - OUTPUT_START][LOC_u8Row - INPUT_START];
-//
-//        /* Wait until the switch is released (Single Press)                                                          */
-//        while (!Dio_u8GetPinValue(KEYPAD_PORT, LOC_u8Row))
-//          ;
-//
-//        /* Delay to avoid bouncing                                                                                   */
-//        _delay_ms(10);
-//
-//      }
-//    }
-//
-//    /* Deactivate the Column                                                                                         */
-//    Dio_vidSetPinValue(KEYPAD_PORT, LOC_u8Column, PIN_HIGH);
-//  }
-//
-//  return LOC_u8Retrun;
-//}
+	for(L_ColumnIndex = 0 ; L_ColumnIndex < 4 ; L_ColumnIndex++)
+	{
 
+		/*Activate the Current Column*/
+		DIO_SetPinDirection(KEYPAD_PORT, L_ColumnArr[L_ColumnIndex] ,PIN_HIGH);
+		_delay_ms(10);
 
+		for(L_RowIndex = 0 ; L_RowIndex < 4 ; L_RowIndex++)
+		{
+			/*Read the Current Row*/
+			L_State = DIO_GetPinValue(KEYPAD_PORT , L_RowArr[L_RowIndex]);
 
-//
-//
-u8 KEYPAD_GetPressedKey(void){
+			/*Check if switch is pressed*/
+			if(PIN_LOW == L_State)
+			{
+				L_PressedKey = btn_values[L_RowIndex][L_ColumnIndex];
 
-	u8 KEY=KEY_NOT_PRESSED;
+				 /* Polling Wait until the switch is released*/
+				while (PIN_LOW == L_State){
 
-	u8 row=0; //input
-	u8 column=0; //output
-	u8 i=0; //iterator
+					L_State = DIO_GetPinValue(KEYPAD_PORT , L_RowArr[L_RowIndex]);
+				}
+				/*avoid bouncing*/
+				_delay_ms(50);
+				/*Deactivate the Current Column*/
+				DIO_SetPinDirection(KEYPAD_PORT, L_ColumnArr[L_ColumnIndex] ,PIN_LOW);
 
-
-	for(column=OUTPUT_START;column<=OUTPUT_END;column++){
-		//deactivate all columns
-		for(i=OUTPUT_START;i<=OUTPUT_END;i++){
-			DIO_SetPinValue(KEYPAD_PORT,i,PIN_HIGH);
-		}
-		//column number is activated;
-		DIO_SetPinValue(KEYPAD_PORT,column,PIN_LOW);
-		//SCAN ROWs
-		for(row=INPUT_START;row<=INPUT_END;row++){
-			if(DIO_GetPinValue(KEYPAD_PORT,row)==PIN_LOW){
-				KEY=btn_values[row-INPUT_START][column-OUTPUT_START];
-				break;
+				return L_PressedKey;
 			}
 		}
+		/*Deactivate the Current Column*/
+		DIO_SetPinDirection(KEYPAD_PORT, L_ColumnArr[L_ColumnIndex] ,PIN_LOW);
+
 	}
 
-
-	return KEY;
+	return L_PressedKey;
 }
--- main.c
+++ main.c
@@ -1,53 +1,389 @@
 /*
  * main.c
  *
- *  Created on: Sep 1, 2023
+ *  Created on: Sep 30, 2023
  *      Author: AhmedSamy
  */
 
 #include "MCAL/DIO/DIO_interface.h"
-#include "HAL/SEVEN_SEGMENTS/hal_segmenets_interface.h"
-#include "HAL/LCD/LCD_Interface.h"
 #include "MCAL/EXTI/EXTI_interface.h"
-#include <avr/io.h>
+#include "MCAL/ADC/ADC_interface.h"
+#include "HAL/LCD/LCD_Interface.h"
+#include "HAL/KEYPAD/hal_keyad_interface.h"
+#include "MCAL/TIMER0/Timer0_interface.h"
+#include "MCAL/PWM/PWM_interface.h"
+#include "MCAL/TIMER1/TIMER1_interface.h"
+#include "MCAL/WATCHDOG/WATCHDOG_register.h"
 
-uint8 data = 0;
-uint8 portstatus = 0;
-
-int main()
-{
-	//LCD_Init();
-	GLOBAL_Interrupt_ENABLE();
-	EXT_Interrupt_Init();
-	EXT_Interrupt_ENABLE(EXTI0);
-	EXT_Interrupt_ENABLE(EXTI1);
+uint8 i = 0 ;
+uint16 periodticks = 0 ;
+uint16 onticks = 0 ;
 
 
-	DIO_SetPinDirection(PORT_D , PIN_0 , PIN_OUTPUT);
-	DIO_SetPinDirection(PORT_D , PIN_1 , PIN_OUTPUT);
+uint8 pressedKey = KEY_NOT_PRESSED;
 
-	DIO_SetPinDirection(PORT_D , PIN_2 , PIN_INPUT);
-	DIO_SetPinValue(PORT_D , PIN_2 , PIN_HIGH);
-	DIO_SetPinDirection(PORT_D , PIN_3 , PIN_INPUT);
-	DIO_SetPinValue(PORT_D , PIN_3 , PIN_HIGH);
-	//LCD_Write_String_Position("Interrupt Lab",1,1);
+uint8 Buzzer = PIN_0;
 
-	while(1)
-	{
+int main (void){
+
+	DIO_SetPinDirection(PORT_B , Buzzer , PIN_OUTPUT);
+
+	LCD_Init();
+	KEYPAD_init();
+
+
+
+	while(1){
+
+
+		while(1)
+		{
+			pressedKey = KEYPAD_GetPressedKey();
+
+			if(pressedKey != KEY_NOT_PRESSED)
+			{
+				LCD_Send_Data(pressedKey);
+				DIO_SetPinValue(PORT_B , Buzzer , PIN_HIGH);
+				_delay_ms(200);
+				DIO_SetPinValue(PORT_B , Buzzer , PIN_LOW);
+				_delay_ms(200);
+
+			}
+			else
+			{
+				/* Do Nothing */
+			}
+		}
 
 	}
-	return 0;
-}
-void __vector_1 (void) __attribute__ ((signal,used, externally_visible)) ;
-    void __vector_1 (void){
-	DIO_TogPinValue(PORT_D, PIN_0);
-	//LCD_Write_String_Position("INT0",2,1);
 }
 
-void __vector_2 (void) __attribute__ ((signal,used, externally_visible)) ;
-	void __vector_2 (void){
-	TOG_BIT(PORTD, PIN_1);
-	//LCD_Write_String_Position("INT1",2,1);
+
+
+/*
+ * icu hw
+ *
+ *
+
+	DIO_SetPinDirection(PORT_B , PIN_3 , PIN_OUTPUT);
+
+	DIO_SetPinDirection(PORT_D , PIN_6 , PIN_INPUT);
+
+
+
+	LCD_Init();
+	LCD_Write_String_Position("HWICU LAB" ,1 , 1 );
+    _delay_ms(1000);
+
+	PWM_Init();
+	PWM_SetMode(NON_INVERTED_MODE);
+	PWM_SetOCRvalue(64);
+
+	TIMER1_init();
+	HWICU_Init();
+	HWICU_SetCallBack(&HWICU);
+	GLOBAL_Interrupt_ENABLE();
+
+
+
+while(1){
+
+
+	LCD_Write_String_Position("Per tick is:" ,1 , 1 );
+	LCD_Send_Number(periodticks);
+
+	LCD_Write_String_Position( "On tick is :" , 2 , 1 );
+	LCD_Send_Number(onticks);
+}
+
+}
+void HWICU()
+{
+
+	static uint8 counter = 0;
+	static uint16 reading1 = 0;
+	static uint16 reading2 = 0;
+	static uint16 reading3 = 0;
+
+	counter ++;
+	if(1 == counter){
+		reading1 = HWICU_ReadTicks();
+	}else if(2 == counter ){
+
+		reading2 = HWICU_ReadTicks();
+		HWICU_EdgeSelect(FALLING_EDGE);
+		periodticks = reading2 - reading1;
+	}else if(3 == counter ){
+
+		reading3 = HWICU_ReadTicks();
+		HWICU_InterruptDisable();
+		onticks = reading3 - reading2;
+
+	}else{
+
+	}
+
+}
+ *
+ *
+ *
+ *
+ *
+ *	SERVO
+ *
+ *		TIMER1_Set_OCR_Value(450);
+		for(uint16 i = 450 ; i <= 2500 ; i++){
+			TIMER1_Set_OCR_Value(i);
+			_delay_ms(1);
+		}
+
+		for(uint16 i = 2500 ; i >= 450 ; i--){
+			TIMER1_Set_OCR_Value(i);
+			_delay_ms(1);
+		}
+
+
+
+
+		ICU
+
+		uint8 i = 0 ;
+ uint16 periodticks = 0 ;
+ uint16 onticks = 0 ;
+
+void SWICU();
+
+int main (void){
+
+	DIO_SetPinDirection(PORT_B , PIN_3 , PIN_OUTPUT);
+	DIO_SetPinDirection(PORT_D , PIN_2 , PIN_INPUT);
+
+	GLOBAL_Interrupt_ENABLE();
+
+	EXTI_Interrupt_Init();
+	EXTI_Interrupt_Set_Callback(SWICU  , EXTI0);
+	EXTI_Interrupt_Set_Mode(EXTI0 ,FALLING_EDGE_MODE  );
+
+	LCD_Init();
+
+
+	PWM_Init();
+	PWM_SetMode(NON_INVERTED_MODE);
+	PWM_SetOCRvalue(64);
+
+	TIMER1_init();
+
+
+	while(1){
+
+		LCD_Write_String_Position("Per tick is:" ,1 , 1 );
+		LCD_Send_Number(periodticks);
+		LCD_Write_String_Position( "On tick is :" , 2 , 1 );
+		LCD_Send_Number(onticks);
+	}
 
 }
 
+void SWICU(){
+	static uint8 counter = 0;
+	counter ++;
+	if(1 == counter){
+		TIMER1_Set_TCNT_Value(0);
+	}else if(2 == counter ){
+
+		periodticks = TIMER1_Read_TCNT_Value();
+		EXTI_Interrupt_Set_Mode(EXTI0 , RISING_EDGE_MODE);
+	}else if(3 == counter ){
+
+		onticks = TIMER1_Read_TCNT_Value();
+		onticks -= periodticks;
+		//EXTI_Interrupt_DISABLE(EXTI0);
+		SET_BIT(GICR , INT0);
+	}else{
+
+	}
+}
+
+
+
+
+
+ * GLOBAL_Interrupt_ENABLE();
+	DIO_SetPinDirection(PORT_B , PIN_3 , PIN_OUTPUT);
+
+
+	PWM_Init();
+	PWM_SetMode(NON_INVERTED_MODE);
+
+	ADC_Init(0);
+
+	uint32 Reading = 0;
+
+	uint16 Analog = 0;
+
+
+	while(1){
+		Reading = ADC_GetChannelReading(0);
+		Analog = (Reading*5000UL) / 1024;
+
+		i = (Analog * 256)/5000UL;
+		TIMER0_SetOCRvalue(i);
+		//_delay_ms(20);
+
+
+	}
+ * PWM SW
+ *
+ *
+void LED();
+
+int main (void){
+	LCD_Init();
+	GLOBAL_Interrupt_ENABLE();
+	DIO_SetPinDirection(PORTA_register , PIN_0 , PIN_OUTPUT);
+	DIO_SetPinDirection(PORTA_register , PIN_1 , PIN_OUTPUT);
+	DIO_SetPinDirection(PORTA_register , PIN_2 , PIN_OUTPUT);
+
+	TIMER0_SetOCRvalue(100);
+	TIMER0_SetCallBackOVF(&LED);
+
+
+	while(1){
+
+
+
+
+	}
+
+}
+
+void LED(){
+	static uint16 counter = 0;
+	counter++;
+	if(3 == counter){
+
+		DIO_SetPinValue(PORTA_register , PIN_0 , PIN_LOW);
+
+
+	}else if(counter == 5) {
+		DIO_SetPinValue(PORTA_register , PIN_1 , PIN_LOW);
+	}else if(counter == 7) {
+		DIO_SetPinValue(PORTA_register , PIN_2 , PIN_LOW);
+	}else if(counter == 10) {
+		DIO_SetPinValue(PORTA_register , PIN_0 , PIN_HIGH);
+		DIO_SetPinValue(PORTA_register , PIN_1 , PIN_HIGH);
+		DIO_SetPinValue(PORTA_register , PIN_2 , PIN_HIGH);
+		counter = 0;
+	}
+}
+
+ *
+ *
+ *
+ * */
+/*Temperture sensor
+ *
+ *
+
+#include "MCAL/DIO/DIO_interface.h"
+#include "MCAL/EXTI/EXTI_interface.h"
+#include "MCAL/ADC/ADC_interface.h"
+#include "HAL/LCD/LCD_Interface.h"
+
+
+int main (void){
+	LCD_Init();
+	GLOBAL_Interrupt_ENABLE();
+	ADC_Init(0);
+
+
+	uint32 Reading = 0;
+
+	uint16 Analog = 0;
+
+	LCD_Write_String("Temperture:");
+
+	while(1){
+		Reading = ADC_GetChannelReading(0);
+		Analog = (Reading*5000UL) / 1024;
+		LCD_Set_Cursor(1 , 13);
+		LCD_Send_Number(Analog/10);
+
+
+	}
+
+}
+game ::
+
+
+
+
+int main (void){
+	LCD_Init();
+	GLOBAL_Interrupt_ENABLE();
+	ADC_Init(0);
+
+
+	uint32 Reading = 0;
+
+	uint16 Analog = 0;
+
+	//LCD_Write_String_Position("Chase Game !",1,2);
+
+	while(1){
+		LCD_Write_String_Position("Chase Game !",1,3);
+
+		Reading = ADC_GetChannelReading(0);
+		Analog = (Reading*500UL) / 1024;
+		Analog = (Analog*16)/490;
+		LCD_Write_String_Position("&",2,Analog);
+		_delay_ms(1000);
+		LCD_Send_Command(_LCD_CLEAR);
+
+
+
+	}
+
+}
+
+ */
+
+/*
+#include "MCAL/DIO/DIO_interface.h"
+#include "MCAL/EXTI/EXTI_interface.h"
+#include "MCAL/ADC/ADC_interface.h"
+#include "HAL/LCD/LCD_Interface.h"
+#include "MCAL/TIMER0/Timer0_interface.h"
+
+void led();
+int main (void){
+
+	GLOBAL_Interrupt_ENABLE();
+
+	DIO_SetPinDirection(PORTA_register , 0 , PIN_OUTPUT);
+
+	TIMER0_SetOCRvalue(250);
+	TIMER0_voidInit();
+	(void)TIMER0_SetCallBack(&led);
+
+	while(1){
+
+	}
+
+}
+
+void led()
+{
+	static uint16 counter = 0 ;
+	static uint8 Led_State = 0;
+	counter ++;
+	if(4000 == counter){
+		TOG_BIT(PORTA_register , 0);
+		//DIO_SetPinValue(PORTA_register , 0 ,Led_State);
+		counter = 0;
+	}
+}
+
+
+
+*/
